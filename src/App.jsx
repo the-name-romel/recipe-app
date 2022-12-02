c4749640d46f284614e7131c7786ccf6f899d2e0
Romel Acopra <racopra@sorsu.edu.ph>
2022-12-02 16:22:50 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const APP_ID = "8480a14e";
+  const APP_KEY = "67dce4413fd18c5470cd6e44b56d8d3d";
+
+  return <div className="App"></div>;
+}
+
+export default App;
